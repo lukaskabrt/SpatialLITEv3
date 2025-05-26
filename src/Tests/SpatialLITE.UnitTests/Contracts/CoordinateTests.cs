9290e1191f1d757e569d251f70df673a60f60ec5
Lukas Kabrt <lukas@kabrt.cz>
2025-05-26 20:51:56 +0200

    Add contracts for Coordinate and Envelope

---
+++ src/Tests/SpatialLITE.UnitTests/Contracts/CoordinateTests.cs
@@ -0,0 +1,18 @@
+﻿using SpatialLITE.Contracts;
+
+namespace SpatialLITE.UnitTests.Contracts;
+
+public class CoordinateTests
+{
+    private readonly double _xCoordinate = 3.5;
+    private readonly double _yCoordinate = 4.2;
+
+    [Fact]
+    public void Constructor_XY_SetsXY()
+    {
+        var target = new Coordinate(_xCoordinate, _yCoordinate);
+
+        Assert.Equal(_xCoordinate, target.X);
+        Assert.Equal(_yCoordinate, target.Y);
+    }
+}
