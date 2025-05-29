75bd07f3f92495e7fc67d6025cadf6dde73cfd00
Lukas Kabrt <lukas@kabrt.cz>
2025-05-29 20:02:58 +0200

    Code cleanup

--- src/Tests/SpatialLITE.UnitTests/Core/IO/WkbWriterTests.cs
+++ src/Tests/SpatialLITE.UnitTests/Core/IO/WkbWriterTests.cs
@@ -6,7 +6,6 @@ namespace SpatialLITE.UnitTests.Core.IO;
 
 public class WkbWriterTests
 {
-
     [Fact]
     public void Constructor_StreamSettings_SetsSettings()
     {
@@ -33,22 +32,16 @@ public class WkbWriterTests
     public void Constructor_PathSettings_SetsSettings()
     {
         var settings = new WkbWriterSettings();
-        using (var target = new WkbWriter(new MemoryStream(), settings))
-        {
-            Assert.Same(settings, target.Settings);
-        }
+        using var target = new WkbWriter(new MemoryStream(), settings);
+        Assert.Same(settings, target.Settings);
     }
 
     [Fact]
     public void Constructor_PathSettings_CreatesOutputFile()
     {
-        string filename = Path.GetTempFileName();
-
+        var filename = Path.GetTempFileName();
         var settings = new WkbWriterSettings();
-        using (var target = new WkbWriter(filename, settings))
-        {
-        }
-
+        using var target = new WkbWriter(filename, settings);
         Assert.True(File.Exists(filename));
     }
 
@@ -62,289 +55,252 @@ public class WkbWriterTests
     [Fact]
     public void Constructor_PathSettings_ThrowsArgumentNullExceptionIfSettingsIsNull()
     {
-        string path = Path.GetTempPath();
-
+        var path = Path.GetTempPath();
         Assert.Throws<ArgumentNullException>(() => new WkbWriter(path, null!));
     }
 
     [Fact]
     public void Constructor_ThrowsExceptionIfEncodingIsSetToBigEndian()
     {
-        var stream = new MemoryStream();
-        Assert.Throws<NotSupportedException>(() => new WkbWriter(stream, new WkbWriterSettings() { Encoding = BinaryEncoding.BigEndian }));
+        using var stream = new MemoryStream();
+        Assert.Throws<NotSupportedException>(() => new WkbWriter(stream, new WkbWriterSettings { Encoding = BinaryEncoding.BigEndian }));
     }
 
     [Fact]
     public void WkbWriter_Write_WritesLittleEndianEncodingByte()
     {
-        var stream = new MemoryStream();
-        using (var target = new WkbWriter(stream, new WkbWriterSettings() { Encoding = BinaryEncoding.LittleEndian }))
-        {
-            target.Write(new Point());
+        using var stream = new MemoryStream();
+        using var target = new WkbWriter(stream, new WkbWriterSettings { Encoding = BinaryEncoding.LittleEndian });
 
-            AssertBytesEqual(stream.ToArray(), 0, [(byte)BinaryEncoding.LittleEndian]);
-        }
+        target.Write(new Point());
+
+        AssertBytesEqual(stream.ToArray(), 0, [(byte)BinaryEncoding.LittleEndian]);
     }
 
     [Fact]
     public void Write_WritesEmptyPointAsEmptyGeometryCollection()
     {
-        var wkt = "point empty";
-        var point = (Point)ParseWKT(wkt);
+        var point = (Point)ParseWKT("point empty");
 
-        var stream = new MemoryStream();
-        using (var target = new WkbWriter(stream, new WkbWriterSettings()))
-        {
-            target.Write(point);
+        using var stream = new MemoryStream();
+        using var target = new WkbWriter(stream, new WkbWriterSettings());
 
-            AssertBytesEqual(stream.ToArray(), TestDataReader.Read("collection-empty.wkb"));
-        }
+        target.Write(point);
+
+        AssertBytesEqual(stream.ToArray(), TestDataReader.Read("collection-empty.wkb"));
     }
 
     [Fact]
     public void Write_Writes2DPoint()
     {
-        var wkt = "point (-10.1 15.5)";
-        var point = (Point)ParseWKT(wkt);
+        var point = (Point)ParseWKT("point (-10.1 15.5)");
 
-        var stream = new MemoryStream();
-        using (var target = new WkbWriter(stream, new WkbWriterSettings()))
-        {
-            target.Write(point);
+        using var stream = new MemoryStream();
+        using var target = new WkbWriter(stream, new WkbWriterSettings());
 
-            AssertBytesEqual(stream.ToArray(), TestDataReader.Read("point-2D.wkb"));
-        }
+        target.Write(point);
+
+        AssertBytesEqual(stream.ToArray(), TestDataReader.Read("point-2D.wkb"));
     }
 
     [Fact]
     public void Write_WritesEmptyLineString()
     {
-        var wkt = "linestring empty";
-        var linestring = (LineString)ParseWKT(wkt);
+        var linestring = (LineString)ParseWKT("linestring empty");
 
-        var stream = new MemoryStream();
-        using (var target = new WkbWriter(stream, new WkbWriterSettings()))
-        {
-            target.Write(linestring);
+        using var stream = new MemoryStream();
+        using var target = new WkbWriter(stream, new WkbWriterSettings());
 
-            AssertBytesEqual(stream.ToArray(), TestDataReader.Read("linestring-empty.wkb"));
-        }
+        target.Write(linestring);
+
+        AssertBytesEqual(stream.ToArray(), TestDataReader.Read("linestring-empty.wkb"));
     }
 
     [Fact]
     public void Write_Writes2DLineString()
     {
-        var wkt = "linestring (-10.1 15.5, 20.2 -25.5, 30.3 35.5)";
-        var linestring = (LineString)ParseWKT(wkt);
+        var linestring = (LineString)ParseWKT("linestring (-10.1 15.5, 20.2 -25.5, 30.3 35.5)");
 
-        var stream = new MemoryStream();
-        using (var target = new WkbWriter(stream, new WkbWriterSettings()))
-        {
-            target.Write(linestring);
+        using var stream = new MemoryStream();
+        using var target = new WkbWriter(stream, new WkbWriterSettings());
 
-            AssertBytesEqual(stream.ToArray(), TestDataReader.Read("linestring-2D.wkb"));
-        }
+        target.Write(linestring);
+
+        AssertBytesEqual(stream.ToArray(), TestDataReader.Read("linestring-2D.wkb"));
     }
 
     [Fact]
     public void Write_WritesEmptyPolygon()
     {
-        var wkt = "polygon empty";
-        var polygon = (Polygon)ParseWKT(wkt);
+        var polygon = (Polygon)ParseWKT("polygon empty");
 
-        var stream = new MemoryStream();
-        using (var target = new WkbWriter(stream, new WkbWriterSettings()))
-        {
-            target.Write(polygon);
+        using var stream = new MemoryStream();
+        using var target = new WkbWriter(stream, new WkbWriterSettings());
 
-            AssertBytesEqual(stream.ToArray(), TestDataReader.Read("polygon-empty.wkb"));
-        }
+        target.Write(polygon);
+
+        AssertBytesEqual(stream.ToArray(), TestDataReader.Read("polygon-empty.wkb"));
     }
 
     [Fact]
     public void Write_Writes2DPolygonOnlyExteriorRing()
     {
-        var wkt = "polygon ((-10.1 15.5, 20.2 -25.5, 30.3 35.5))";
-        var polygon = (Polygon)ParseWKT(wkt);
+        var polygon = (Polygon)ParseWKT("polygon ((-10.1 15.5, 20.2 -25.5, 30.3 35.5))");
 
-        var stream = new MemoryStream();
-        using (var target = new WkbWriter(stream, new WkbWriterSettings()))
-        {
-            target.Write(polygon);
+        using var stream = new MemoryStream();
+        using var target = new WkbWriter(stream, new WkbWriterSettings());
 
-            AssertBytesEqual(stream.ToArray(), TestDataReader.Read("polygon-ext-2D.wkb"));
-        }
+        target.Write(polygon);
+
+        AssertBytesEqual(stream.ToArray(), TestDataReader.Read("polygon-ext-2D.wkb"));
     }
 
     [Fact]
     public void Write_WritesEmptyMultipoint()
     {
-        var wkt = "multipoint empty";
-        var multipoint = (MultiPoint)ParseWKT(wkt);
+        var multipoint = (MultiPoint)ParseWKT("multipoint empty");
 
-        var stream = new MemoryStream();
-        using (var target = new WkbWriter(stream, new WkbWriterSettings()))
-        {
-            target.Write(multipoint);
+        using var stream = new MemoryStream();
+        using var target = new WkbWriter(stream, new WkbWriterSettings());
 
-            AssertBytesEqual(stream.ToArray(), TestDataReader.Read("multipoint-empty.wkb"));
-        }
+        target.Write(multipoint);
+
+        AssertBytesEqual(stream.ToArray(), TestDataReader.Read("multipoint-empty.wkb"));
     }
 
     [Fact]
     public void Write_Writes2DMultiPoint()
     {
-        var wkt = "multipoint ((-10.1 15.5),(20.2 -25.5))";
-        var multipoint = (MultiPoint)ParseWKT(wkt);
+        var multipoint = (MultiPoint)ParseWKT("multipoint ((-10.1 15.5),(20.2 -25.5))");
 
-        var stream = new MemoryStream();
-        using (var target = new WkbWriter(stream, new WkbWriterSettings()))
-        {
-            target.Write(multipoint);
+        using var stream = new MemoryStream();
+        using var target = new WkbWriter(stream, new WkbWriterSettings());
 
-            AssertBytesEqual(stream.ToArray(), TestDataReader.Read("multipoint-2D.wkb"));
-        }
+        target.Write(multipoint);
+
+        AssertBytesEqual(stream.ToArray(), TestDataReader.Read("multipoint-2D.wkb"));
     }
 
     [Fact]
     public void Write_WritesEmptyMultiLineString()
     {
-        var wkt = "multilinestring empty";
-        var multilinestring = (MultiLineString)ParseWKT(wkt);
+        var multilinestring = (MultiLineString)ParseWKT("multilinestring empty");
 
-        var stream = new MemoryStream();
-        using (var target = new WkbWriter(stream, new WkbWriterSettings()))
-        {
-            target.Write(multilinestring);
+        using var stream = new MemoryStream();
+        using var target = new WkbWriter(stream, new WkbWriterSettings());
 
-            AssertBytesEqual(stream.ToArray(), TestDataReader.Read("multilinestring-empty.wkb"));
-        }
+        target.Write(multilinestring);
+
+        AssertBytesEqual(stream.ToArray(), TestDataReader.Read("multilinestring-empty.wkb"));
     }
 
     [Fact]
     public void Write_Writes2DMultiLineString()
     {
-        var wkt = "multilinestring ((-10.1 15.5, 20.2 -25.5, 30.3 35.5),(-10.1 15.5, 20.2 -25.5, 30.3 35.5))";
-        var multilinestring = (MultiLineString)ParseWKT(wkt);
+        var multilinestring = (MultiLineString)ParseWKT("multilinestring ((-10.1 15.5, 20.2 -25.5, 30.3 35.5),(-10.1 15.5, 20.2 -25.5, 30.3 35.5))");
 
-        var stream = new MemoryStream();
-        using (var target = new WkbWriter(stream, new WkbWriterSettings()))
-        {
-            target.Write(multilinestring);
+        using var stream = new MemoryStream();
+        using var target = new WkbWriter(stream, new WkbWriterSettings());
 
-            AssertBytesEqual(stream.ToArray(), TestDataReader.Read("multilinestring-2D.wkb"));
-        }
+        target.Write(multilinestring);
+
+        AssertBytesEqual(stream.ToArray(), TestDataReader.Read("multilinestring-2D.wkb"));
     }
 
     [Fact]
     public void Write_WritesEmptyMultiPolygon()
     {
-        var wkt = "multipolygon empty";
-        var multipolygon = (MultiPolygon)ParseWKT(wkt);
+        var multipolygon = (MultiPolygon)ParseWKT("multipolygon empty");
 
-        var stream = new MemoryStream();
-        using (var target = new WkbWriter(stream, new WkbWriterSettings()))
-        {
-            target.Write(multipolygon);
+        using var stream = new MemoryStream();
+        using var target = new WkbWriter(stream, new WkbWriterSettings());
 
-            AssertBytesEqual(stream.ToArray(), TestDataReader.Read("multipolygon-empty.wkb"));
-        }
+        target.Write(multipolygon);
+
+        AssertBytesEqual(stream.ToArray(), TestDataReader.Read("multipolygon-empty.wkb"));
     }
 
     [Fact]
     public void Write_Writes2DMultiPolygon()
     {
-        var wkt = "multipolygon (((-10.1 15.5, 20.2 -25.5, 30.3 35.5)),((-10.1 15.5, 20.2 -25.5, 30.3 35.5)))";
-        var multipolygon = (MultiPolygon)ParseWKT(wkt);
+        var multipolygon = (MultiPolygon)ParseWKT("multipolygon (((-10.1 15.5, 20.2 -25.5, 30.3 35.5)),((-10.1 15.5, 20.2 -25.5, 30.3 35.5)))");
 
-        var stream = new MemoryStream();
-        using (var target = new WkbWriter(stream, new WkbWriterSettings()))
-        {
-            target.Write(multipolygon);
+        using var stream = new MemoryStream();
+        using var target = new WkbWriter(stream, new WkbWriterSettings());
 
-            AssertBytesEqual(stream.ToArray(), TestDataReader.Read("multipolygon-2D.wkb"));
-        }
+        target.Write(multipolygon);
+
+        AssertBytesEqual(stream.ToArray(), TestDataReader.Read("multipolygon-2D.wkb"));
     }
 
     [Fact]
     public void Write_WritesEmptyGeometryCollection()
     {
-        var wkt = "geometrycollection empty";
-        var collection = (GeometryCollection<Geometry>)ParseWKT(wkt);
+        var collection = (GeometryCollection<Geometry>)ParseWKT("geometrycollection empty");
 
-        var stream = new MemoryStream();
-        using (var target = new WkbWriter(stream, new WkbWriterSettings()))
-        {
-            target.Write(collection);
+        using var stream = new MemoryStream();
+        using var target = new WkbWriter(stream, new WkbWriterSettings());
 
-            AssertBytesEqual(stream.ToArray(), TestDataReader.Read("collection-empty.wkb"));
-        }
+        target.Write(collection);
+
+        AssertBytesEqual(stream.ToArray(), TestDataReader.Read("collection-empty.wkb"));
     }
 
     [Fact]
     public void Write_Writes2DGeometryCollection()
     {
-        var wkt = "geometrycollection (point (-10.1 15.5))";
-        var collection = (GeometryCollection<Geometry>)ParseWKT(wkt);
+        var collection = (GeometryCollection<Geometry>)ParseWKT("geometrycollection (point (-10.1 15.5))");
 
-        var stream = new MemoryStream();
-        using (var target = new WkbWriter(stream, new WkbWriterSettings()))
-        {
-            target.Write(collection);
+        using var stream = new MemoryStream();
+        using var target = new WkbWriter(stream, new WkbWriterSettings());
 
-            AssertBytesEqual(stream.ToArray(), TestDataReader.Read("collection-2D.wkb"));
-        }
+        target.Write(collection);
+
+        AssertBytesEqual(stream.ToArray(), TestDataReader.Read("collection-2D.wkb"));
     }
 
     [Fact]
     public void Write_WritesCollectionWithPointLineStringAndPolygon()
     {
-        var wkt = "geometrycollection (point (-10.1 15.5),linestring (-10.1 15.5, 20.2 -25.5, 30.3 35.5),polygon ((-10.1 15.5, 20.2 -25.5, 30.3 35.5)))";
-        var collection = (GeometryCollection<Geometry>)ParseWKT(wkt);
+        var collection = (GeometryCollection<Geometry>)ParseWKT("geometrycollection (point (-10.1 15.5),linestring (-10.1 15.5, 20.2 -25.5, 30.3 35.5),polygon ((-10.1 15.5, 20.2 -25.5, 30.3 35.5)))");
 
-        var stream = new MemoryStream();
-        using (var target = new WkbWriter(stream, new WkbWriterSettings()))
-        {
-            target.Write(collection);
+        using var stream = new MemoryStream();
+        using var target = new WkbWriter(stream, new WkbWriterSettings());
 
-            AssertBytesEqual(stream.ToArray(), TestDataReader.Read("collection-pt-ls-poly.wkb"));
-        }
+        target.Write(collection);
+
+        AssertBytesEqual(stream.ToArray(), TestDataReader.Read("collection-pt-ls-poly.wkb"));
     }
 
     [Fact]
     public void Write_WritesCollectionWithMultiGeometries()
     {
-        var wkt = "geometrycollection (multipoint empty,multilinestring empty,multipolygon empty)";
-        var collection = (GeometryCollection<Geometry>)ParseWKT(wkt);
+        var collection = (GeometryCollection<Geometry>)ParseWKT("geometrycollection (multipoint empty,multilinestring empty,multipolygon empty)");
 
-        var stream = new MemoryStream();
-        using (var target = new WkbWriter(stream, new WkbWriterSettings()))
-        {
-            target.Write(collection);
+        using var stream = new MemoryStream();
+        using var target = new WkbWriter(stream, new WkbWriterSettings());
 
-            AssertBytesEqual(stream.ToArray(), TestDataReader.Read("collection-multi.wkb"));
-        }
+        target.Write(collection);
+
+        AssertBytesEqual(stream.ToArray(), TestDataReader.Read("collection-multi.wkb"));
     }
 
     [Fact]
     public void Write_WritesNestedCollection()
     {
-        var wkt = "geometrycollection (geometrycollection (point (-10.1 15.5)))";
-        var collection = (GeometryCollection<Geometry>)ParseWKT(wkt);
+        var collection = (GeometryCollection<Geometry>)ParseWKT("geometrycollection (geometrycollection (point (-10.1 15.5)))");
 
-        var stream = new MemoryStream();
-        using (var target = new WkbWriter(stream, new WkbWriterSettings()))
-        {
-            target.Write(collection);
+        using var stream = new MemoryStream();
+        using var target = new WkbWriter(stream, new WkbWriterSettings());
 
-            AssertBytesEqual(stream.ToArray(), TestDataReader.Read("collection-nested.wkb"));
-        }
+        target.Write(collection);
+
+        AssertBytesEqual(stream.ToArray(), TestDataReader.Read("collection-nested.wkb"));
     }
 
     private static void AssertBytesEqual(byte[] array, byte[] expected)
     {
         Assert.Equal(array.Length, expected.Length);
-
         for (var i = 0; i < expected.Length; i++)
         {
             Assert.Equal(expected[i], array[i]);
@@ -353,14 +309,11 @@ public class WkbWriterTests
 
     private static void AssertBytesEqual(byte[] array, int offset, byte[] expected)
     {
-        for (var i = 0; i < expected.Length; i++)
+        foreach (var (expectedByte, i) in expected.Select((b, i) => (b, i)))
         {
-            Assert.Equal(expected[i], array[i + offset]);
+            Assert.Equal(expectedByte, array[i + offset]);
         }
     }
 
-    private static Geometry ParseWKT(string wkt)
-    {
-        return WktReader.Parse(wkt)!;
-    }
+    private static Geometry ParseWKT(string wkt) => WktReader.Parse(wkt)!;
 }
