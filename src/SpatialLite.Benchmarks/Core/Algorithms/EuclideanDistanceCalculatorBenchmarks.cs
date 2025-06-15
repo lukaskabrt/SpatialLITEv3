49cc7b5f09b40e7b451e2bf4c28ab34623b84a6e
Lukas Kabrt <lukas@kabrt.cz>
2025-06-15 20:22:45 +0200

    Benchmarks refactoring

--- src/SpatialLite.Benchmarks/Core/Algorithms/EuclideanDistanceCalculatorBenchmarks.cs
+++ src/SpatialLite.Benchmarks/Core/Algorithms/EuclideanDistanceCalculatorBenchmarks.cs
@@ -11,113 +11,43 @@ public class EuclideanDistanceCalculatorBenchmarks
     private readonly EuclideanDistanceCalculator _calculator = new();
 
     // Test data for benchmarks
-    private readonly Coordinate _testPoint = new(5.0, 10.0);
-    private readonly Coordinate _linePointA = new(0.0, 0.0);
-    private readonly Coordinate _linePointB = new(10.0, 0.0);
-
-    // Collections for different N values
-    private Coordinate[] _points1 = [];
-    private Coordinate[] _points10 = [];
-    private Coordinate[] _points100 = [];
-
-    private (Coordinate A, Coordinate B)[] _lines1 = [];
-    private (Coordinate A, Coordinate B)[] _lines10 = [];
-    private (Coordinate A, Coordinate B)[] _lines100 = [];
-
-    [Params(1, 10, 100)]
-    public int N { get; set; }
-
-    [GlobalSetup]
-    public void Setup()
-    {
-        // Setup points for point-to-point benchmarks
-        _points1 = [new(1.0, 1.0)];
-
-        _points10 = new Coordinate[10];
-        for (int i = 0; i < 10; i++)
-        {
-            _points10[i] = new(i * 2.0, i * 3.0);
-        }
-
-        _points100 = new Coordinate[100];
-        for (int i = 0; i < 100; i++)
-        {
-            _points100[i] = new(i * 0.5, i * 0.7);
-        }
-
-        // Setup lines for point-to-line benchmarks
-        _lines1 = [(new(0.0, 0.0), new(10.0, 0.0))];
-
-        _lines10 = new (Coordinate A, Coordinate B)[10];
-        for (int i = 0; i < 10; i++)
-        {
-            _lines10[i] = (new(i * 2.0, i * 2.0), new(i * 2.0 + 5.0, i * 2.0 + 3.0));
-        }
-
-        _lines100 = new (Coordinate A, Coordinate B)[100];
-        for (int i = 0; i < 100; i++)
-        {
-            _lines100[i] = (new(i * 0.5, i * 0.3), new(i * 0.5 + 2.0, i * 0.3 + 1.5));
-        }
-    }
+    private static readonly Coordinate C = new(5.0, 10.0);
+    private static readonly Coordinate A = new(3.0, 5.0);
+    private static readonly Coordinate B = new(-3.0, -5.0);
 
     [Benchmark]
     public double PointToPoint()
     {
-        var points = N switch
+        var distance = 0.0;
+        for (var i = 0; i < 1000; i++)
         {
-            1 => _points1,
-            10 => _points10,
-            100 => _points100,
-            _ => throw new ArgumentException("Invalid N value")
-        };
-
-        double totalDistance = 0.0;
-        foreach (var point in points)
-        {
-            totalDistance += _calculator.CalculateDistance(_testPoint, point);
+            distance += _calculator.CalculateDistance(C, A);
         }
 
-        return totalDistance;
+        return distance;
     }
 
     [Benchmark]
     public double PointToLine()
     {
-        var lines = N switch
+        var distance = 0.0;
+        for (var i = 0; i < 1000; i++)
         {
-            1 => _lines1,
-            10 => _lines10,
-            100 => _lines100,
-            _ => throw new ArgumentException("Invalid N value")
-        };
-
-        double totalDistance = 0.0;
-        foreach (var (a, b) in lines)
-        {
-            totalDistance += _calculator.CalculateDistance(_testPoint, a, b, LineMode.Line);
+            distance += _calculator.CalculateDistance(C, A, B, LineMode.Line);
         }
 
-        return totalDistance;
+        return distance;
     }
 
     [Benchmark]
     public double PointToLineSegment()
     {
-        var lines = N switch
+        var distance = 0.0;
+        for (var i = 0; i < 1000; i++)
         {
-            1 => _lines1,
-            10 => _lines10,
-            100 => _lines100,
-            _ => throw new ArgumentException("Invalid N value")
-        };
-
-        double totalDistance = 0.0;
-        foreach (var (a, b) in lines)
-        {
-            totalDistance += _calculator.CalculateDistance(_testPoint, a, b, LineMode.LineSegment);
+            distance += _calculator.CalculateDistance(C, A, B, LineMode.LineSegment);
         }
 
-        return totalDistance;
+        return distance;
     }
 }
--- src/SpatialLite.Benchmarks/Program.cs
+++ src/SpatialLite.Benchmarks/Program.cs
@@ -1,14 +1,18 @@
-﻿using BenchmarkDotNet.Running;
-//using SpatialLite.Benchmarks.Contracts;
-//using SpatialLite.Benchmarks.Core.Algorithms;
-//using SpatialLite.Benchmarks.Core.IO;
+﻿#pragma warning disable IDE0005
+
+using BenchmarkDotNet.Running;
+using SpatialLite.Benchmarks.Contracts;
+using SpatialLite.Benchmarks.Core.Algorithms;
+using SpatialLite.Benchmarks.Core.IO;
 using SpatialLite.Benchmarks.Osm.IO;
 
+#pragma warning restore IDE0005
+
 //BenchmarkRunner.Run<EnvelopeBenchmarks>();
-//BenchmarkRunner.Run<EuclideanDistanceCalculatorBenchmarks>();
+BenchmarkRunner.Run<EuclideanDistanceCalculatorBenchmarks>();
 //BenchmarkRunner.Run<WktReaderBenchmarks>();
 //BenchmarkRunner.Run<WktWriterBenchmarks>();
 //BenchmarkRunner.Run<OsmXmlReaderBenchmarks>();
 //BenchmarkRunner.Run<OsmXmlWriterBenchmarks>();
-BenchmarkRunner.Run<PbfReaderBenchmarks>();
+//BenchmarkRunner.Run<PbfReaderBenchmarks>();
 //BenchmarkRunner.Run<PbfWriterBenchmarks>();
